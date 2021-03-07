d1836421536c2fdf67b471119554e79eb916aced
ninja8bpyt <78526646+ninja8bpyt@users.noreply.github.com>
2021-03-07 16:54:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,8 +85,7 @@ async def home(client, message):
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
-        InlineKeyboardButton('Our Channel', url='http://telegram.me/indusbots'),
-        InlineKeyboardButton('Source Code', url='https://github.com/benchamxd/Telegraph-Uploader')
+        InlineKeyboardButton('Our Channel', url='http://telegram.me/bots_ki_duniya')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
@@ -97,11 +96,11 @@ im a telegraph Uploader That Can Upload Photo, Video And Gif
         
 Simply send me photo, video or gif to upload to Telegra.ph
         
-Made With Love By @indusBots</b>""",
+Made With Love By @Bots_Ki_Duniya</b>""",
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=message.message_id
-    )
+
 
 @Tgraph.on_message(filters.command(["help"]))
 async def help(client, message):
@@ -110,14 +109,14 @@ async def help(client, message):
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
-        InlineKeyboardButton('Our Channel', url='http://telegram.me/indusbots')
+        InlineKeyboardButton('Our Channel', url='http://telegram.me/bots_ki_duniya')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
-        text="""There Is Nothung To KnowMore,
+        text="""There Is Nothung To Know More,
         
-Just Send Me A Video/gif/photo Upto 5mb.
+Just Send Me A Video/GIF/Photo Upto 5mb.
 
 i'll upload ut to telegra.ph and give you the direct link""",
         reply_markup=reply_markup,
